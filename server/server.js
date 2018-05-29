18596d35b1976b31de15354590b2795c4aaed5dc
Roman <vizvoxer@gmail.com>
2018-05-29 23:51:29 +0300

    Update

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const app = express();
 app.use(express.static("public"));
 const path = require("path");
 const publicPath = path.join(__dirname, '/../public');
-
-app.listen(3000, () => {
-    console.log("listening on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
 
 app.get("/", (req,res) => {
